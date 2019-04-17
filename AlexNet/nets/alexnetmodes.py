a04a99e39dc8ac168dcd77fb085774038ca30711
Maulik <shahms95@gmail.com>
2019-04-17 17:12:42 -0500

    testing

--- AlexNet/nets/alexnetmodes.py
+++ AlexNet/nets/alexnetmodes.py
@@ -113,8 +113,8 @@ def distribute(images, labels, num_classes, total_num_examples, devices, is_trai
     with tf.device(tf.train.replica_device_setter()):
         builder = ModelBuilder()
         print('num_classes: ' + str(num_classes))
-        with tf.variable_scope("scope-{}".format(i)):
-            net, logits, total_loss = alexnet_inference(builder, images, labels, num_classes)
+        # with tf.variable_scope("scope-{}".format(i)):
+        net, logits, total_loss = alexnet_inference(builder, images, labels, num_classes)
 
         if not is_train:
             return alexnet_eval(net, labels)
