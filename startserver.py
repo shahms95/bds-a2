ff30405534fec838f5da8ba727fb6528b0869674
Maulik <shahms95@gmail.com>
2019-04-17 18:27:46 -0500

    testing

--- startserver.py
+++ startserver.py
@@ -10,7 +10,7 @@ tf.logging.set_verbosity(tf.logging.DEBUG)
 
 clusterSpec_single = tf.train.ClusterSpec({
     "worker" : [
-        # "c220g5-110516.wisc.cloudlab.us:2222"
+        # "c220g5-120102.wisc.cloudlab.us:2222"
         "localhost:2222"
     ]
 })
@@ -18,23 +18,23 @@ clusterSpec_single = tf.train.ClusterSpec({
 clusterSpec_cluster = tf.train.ClusterSpec({
     "ps" : [
         "localhost:2223"
-        # "c220g5-110516.wisc.cloudlab.us:2223"
+        # "c220g5-120102.wisc.cloudlab.us:2223"
     ],
     "worker" : [
-        "c220g5-110516.wisc.cloudlab.us:2222",
-        "c220g5-111032.wisc.cloudlab.us:2222"
+        "c220g5-120102.wisc.cloudlab.us:2222",
+        "c220g5-120118.wisc.cloudlab.us:2222"
     ]
 })
 
 clusterSpec_cluster2 = tf.train.ClusterSpec({
     "ps" : [
         "localhost:2223"
-        # "c220g5-110516.wisc.cloudlab.us:2223"
+        # "c220g5-120102.wisc.cloudlab.us:2223"
     ],
     "worker" : [
-        "c220g5-110516.wisc.cloudlab.us:2222",
-        "c220g5-111032.wisc.cloudlab.us:2222",
-        "c220g5-111011.wisc.cloudlab.us:2222",
+        "c220g5-120102.wisc.cloudlab.us:2222",
+        "c220g5-120118.wisc.cloudlab.us:2222",
+        "c220g5-120106.wisc.cloudlab.us:2222",
     ]
 })
 
