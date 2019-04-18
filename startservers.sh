877427664f3f12da068621292563df7b2be2960c
Maulik <shahms95@gmail.com>
2019-04-17 19:46:23 -0500

    testing

--- startservers.sh
+++ startservers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source cluster_utils.sh
-start_cluster startserver.py single
-# start_cluster startserver.py cluster
+# start_cluster startserver.py single
+start_cluster startserver.py cluster
