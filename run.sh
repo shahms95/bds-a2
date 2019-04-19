5f8ad8f808859d3133cfd75369e5f685acc8ebfb
Maulik <shahms95@gmail.com>
2019-04-19 10:47:29 -0500

    Changes in sync mode optimization

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-export TF_LOG_DIR="/tf/logs/"
-
+export TF_LOG_DIR="tf/tblogs/"
 # run a simple program that generates logs for tensorboard
 # mkdir -p $TF_LOG_DIR
 terminate_cluster
 ./startservers.sh $1
-python -m AlexNet.scripts.train --mode $1
+python -m AlexNet.scripts.train --mode $1 --log_dir $TF_LOG_DIR
 
 # start the tensorboard web server. If you have started the webserver on the VM
 # a public ip, then you can view Tensorboard on the browser on your workstation
