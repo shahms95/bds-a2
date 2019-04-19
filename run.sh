88d53b34ae0880c642871c0a34c6bf14db8c2cf2
Maulik <Maulik@node0.g16-a2.cs744-s19-pg0.wisc.cloudlab.us>
2019-04-19 10:32:52 -0500

    tensorboard

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export TF_LOG_DIR="./tflogs/"
+
+# run a simple program that generates logs for tensorboard
+mkdir -p $TF_LOG_DIR
+terminate_cluster
+./startservers.sh $1
+python -m AlexNet.scripts.train --mode $1
+
+# start the tensorboard web server. If you have started the webserver on the VM
+# a public ip, then you can view Tensorboard on the browser on your workstation
+# (not the CloudLab VMs). Navigate to http://<publicip>:6006 on your browser and
+# look under "GRAPHS" tab.
+
+# Under the "GRAPHS" tab, use the options on the left to navigate to the "Run" you are interested in.
+tensorboard --logdir $TF_LOG_DIR
